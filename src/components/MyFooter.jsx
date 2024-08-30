38b2f5becafddb3679b55c7d75eba618cbb297b4
METAORI <nico97.mecca@gmail.com>
2024-08-30 17:45:45 +0200

    icons

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import 'bootstrap-icons/font/bootstrap-icons.css'; 
 
 
 const MyFooter = () => {
@@ -20,15 +21,19 @@ const MyFooter = () => {
           </Col>
           <Col md="4">
             <h5>Seguici</h5>
-            <a href="https://facebook.com" className="text-white me-2">
-              <i className="fab fa-facebook-f"></i>
-            </a>
-            <a href="https://twitter.com" className="text-white me-2">
-              <i className="fab fa-twitter"></i>
+            <div className ="mt-4">
+            <a href="https://facebook.com" className="text-white me-2 ms-2 fs-3">
+            <i className="bi bi-facebook footer-icon me-2"></i> </a>
+            <a href="https://twitter.com" className="text-white me-2 ms-2 fs-3">
+            <i className="bi bi-twitter-x footer-icon me-2"></i>
             </a>
             <a href="https://instagram.com" className="text-white">
-              <i className="fab fa-instagram"></i>
+            <i className="bi bi-instagram footer-icon me-2 ms-2 fs-3"></i>
             </a>
+            </div>
+            
+            
+          
           </Col>
         </Row>
       </Container>
